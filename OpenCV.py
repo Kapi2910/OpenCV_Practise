d7bf067bf3539c6c5ac2c9195756aac6b33727b5
Aaditya Prakash Kattekola <kattek_811981@student.nitw.ac.in>
2020-12-30 11:51:42 +0400

    BGR and RGB Differences

--- OpenCV.py
+++ OpenCV.py
@@ -1,20 +1,29 @@
 import cv2
+import numpy as npy
+import matplotlib.pyplot as plt
 
-img = cv2.imread('images/logo.png')
-
-dimensions = img.shape  
-size = img.size         # size = height * width * channels
-img_type = img.dtype
-
-(b, r, g) = img[0, 0]
-
-print(
-    f"Dimensions:{dimensions}\n",
-    f"Blue:{b}",
-    f"Green:{g}",
-    f"Red:{r}"
-)
+img_CV = cv2.imread('images/ManCity_Logo.png')
 
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+b, g, r = cv2.split(img_CV)
+
+img_plt = cv2.merge([r, g, b])
+
+def ImageByPlt(img_CV, img_plt):
+    plt.subplot(121)
+    plt.imshow(img_CV)
+    plt.subplot(122)
+    plt.imshow(img_plt)
+    plt.show()
+
+def ImageByCV(img_CV, img_plt):
+    img_concate = npy.concatenate((img_CV, img_plt), axis=1)
+    cv2.imshow( "BGR", img_CV)    
+    cv2.imshow( "RGB", img_plt)    
+    cv2.imshow( "BGR & RGB", img_concate)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+ImageByCV(img_CV, img_plt)    
